4912c8fe67b2768adfe4d4496aa4913c5eb6db3d
murilo_kronbauer <murilokronbauer@gmail.com>
2019-06-07 21:14:44 -0300

    Front end v01

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,11 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = TB_Cliente
         fields = "__all__"
+
+        # widgets = {
+        #     'cpf' : forms.CharField(attrs={'class': 'cpf_class'}),
+        # }
+
+    # def __init__(self, *args, **kwargs):
+    #     super(ClienteForm, self).__init__(*args, **kwargs)
+    #     self.fields["__all__"].widget.attrs.update({'class': 'myfieldclass'})
